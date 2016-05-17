22c75bcef5961bd0731c7f0bfb5c7359998f6ecb
Daniel Meilleur <daniel.meilleur@gmail.com>
2016-05-16 21:37:02 -0700

    image de texas instrument frame

--- js/calc.js
+++ js/calc.js
@@ -28,18 +28,16 @@ var cal = {
     'operation': function(sign) {
         this.acc = this.doCalc(this.nextOp, this.num);
         this.nextOp = signToOperator[sign];
-        console.log('nextOp', this.nextOp);
-        console.log('num', this.num);
-        console.log('acc', this.acc);
-        console.log('sign', sign);
         this.num = '';
         return this.acc;
     },
 
-
 	'ce': function() {
+        /*
 		this.num = '';
 		return 0;
+        */
+        return this.delDigit();
 	},
 	'ac': function() {
 		this.nextOp = '';
@@ -49,8 +47,6 @@ var cal = {
 	},
 	'doCalc': function(operation, x) {
         if (!x) {x = 0;}
-        console.log('oper=', operation, 'this.acc=', this.acc,  typeof this.acc);
-        console.log('oper=', operation, x, typeof x);
 		if (operation === 'add') {
             this.acc = parseFloat(this.acc) + parseFloat(x);
             return this.chopE(this.acc);
@@ -69,12 +65,6 @@ var cal = {
             return this.acc;
         }
 	},
-	'register': function() {
-		arr.push(this.num);
-	},
-	'add': function() {
-		return this.num += 1;
-	},
 	'chopE': function(x) {
 		return Math.round(
                 x * Math.pow(10, this.nbDigits)) / Math.pow(10, this.nbDigits);
@@ -82,49 +72,20 @@ var cal = {
 };
 // jQuery
 $(function() {
-    $('.digit').click(function() {
+    $('.dig-button').click(function() {
        $('#affichage').html(cal.addDigit($(this).html()));
     });
     $('.dot').click(function() {
         $('#affichage').html(cal.dot());
     });
     $('.ce').click(function() {
-        $('#affichage').html(cal.delDigit());
+        $('#affichage').html(cal.ce());
+    });
+    $('.ac').click(function() {
+        $('#affichage').html(cal.ac());
     });
     $('.op').click(function() {
         var sign = $(this).html();
         $('#affichage').html(cal.operation($(this).html()));
     });
 });
-
-// effacer
-        /*
-        if (operator === '+') {
-            this.nextOp = 'add';
-            this.decimal = 0;
-            this.num = '';
-            return this.acc;
-        } else if (operator === '-'){
-            this.nextOp = 'substract';
-            this.decimal = 0;
-            this.num = '';
-            return this.acc;
-        } else if (operator === '*'){
-            this.nextOp = 'multiply';
-            this.decimal = 0;
-            this.num = '';
-            return this.acc;
-        } else if (operator === '/'){
-            this.nextOp = 'divide';
-            this.decimal = 0;
-            this.num = '';
-            return this.acc;
-        } else if (operator === '='){
-            this.nextOp = '';
-            this.decimal = 0;
-            this.num = '';
-            return this.acc;
-        } else {
-            //
-        }
-        */
